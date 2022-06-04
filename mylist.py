5762c77a68c91eb426b3d123c9503bb0232f9817
Trump2003 <skttrung2003@gmail.com>
2022-06-04 22:06:30 +0700

    first commit

---
+++ mylist.py
@@ -0,0 +1,24 @@
+mylist = ["trung","hòa"]
+x= ("anh")
+newlist = mylist.append (x)
+print(f'result after append list:{mylist}')
+Mylist =[0,1,2]
+y= 3
+Newlist = Mylist.insert (1,y)
+print (f'result after insert list:{Mylist}')
+list1= ["mot","hai","ba"]
+list2= [1,2,3]
+list3= list1 +list2
+print (list3)
+print (f'result after index item hai la:   ', list1.index ("hai"))
+thislist=["r", "i", "n"]
+NEWlist = thislist.remove("r")
+print (f'result after remove the first:{thislist}')
+THISlist=["Hue","DN","QN"]
+ourlist= THISlist.sort ()
+print (f'result after sort list:{THISlist} ')
+OURlist= THISlist.sort(reverse=True)
+print (f'result after reverse:{THISlist}')
+THISLIST=[1,2,3,4,5,6]
+NEWLIST = THISLIST.pop ()
+print (f'result after pop is item the last removed:{THISLIST}')
